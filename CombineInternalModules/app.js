bf232465500b7eccc70354fbdb5155e0f06b0f9b
Ilya Verbitskiy <iverbitskiy@gmail.com>
2015-04-20 23:52:26 -0500

    Added example of nested modules

--- CombineInternalModules/app.js
+++ CombineInternalModules/app.js
@@ -13,14 +13,17 @@ var Module1;
 /// <reference path="module1.ts" />
 var Module2;
 (function (Module2) {
-    var Class2 = (function () {
-        function Class2() {
-        }
-        Class2.prototype.test = function () {
-            alert("Module2.Class2");
-        };
-        return Class2;
-    })();
-    Module2.Class2 = Class2;
+    var MyClasses;
+    (function (MyClasses) {
+        var Class2 = (function () {
+            function Class2() {
+            }
+            Class2.prototype.test = function () {
+                alert("Module2.Class2");
+            };
+            return Class2;
+        })();
+        MyClasses.Class2 = Class2;
+    })(MyClasses = Module2.MyClasses || (Module2.MyClasses = {}));
 })(Module2 || (Module2 = {}));
 //# sourceMappingURL=app.js.map
--- CombineInternalModules/module2.ts
+++ CombineInternalModules/module2.ts
@@ -1,9 +1,11 @@
 ﻿/// <reference path="module1.ts" />
 
 module Module2 {
-    export class Class2 implements Module1.Test {
-        test() {
-            alert("Module2.Class2");
+    export module MyClasses {
+        export class Class2 implements Module1.Test {
+            test() {
+                alert("Module2.Class2");
+            }
         }
     }
 } 
